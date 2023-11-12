28812e776692b5503e615fd270795abe91418ecc
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2023-11-13 04:53:17 +0700

    update

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,19 +6,13 @@ import Checkout from "./pages/Checkout"
 import NotFound from "./pages/NotFound"
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        errorElement: <NotFound />,
-        children: [
-            { path: '', element: <Products /> },
-            { path: 'product/:id', element: <Product /> },
-            { path: 'checkout', element: <Checkout /> },
-        ]
-    },
+    { path: '/', element: <Products /> },
+    { path: '/product/:id', element: <Product /> },
+    { path: '/checkout', element: <Checkout /> },
+    { path: '*', element: <NotFound /> },
 ],
     { basename: import.meta.env.DEV ? '/' : '/BorntoDev-FEBC-DAY23/' }
 )
-
 
 export default function Routes() {
     return <RouterProvider router={router} />
